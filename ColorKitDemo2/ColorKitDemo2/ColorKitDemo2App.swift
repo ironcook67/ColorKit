d585ad7730071be86cdcbe3a5887b8d3bd2fc256
Chon Torres <chon@chontorres.com>
2025-06-03 12:46:08 -0700

    added color mixing and color intensity to named colors (#1)
    - Added color mixing to named colors. This allows building Colors with combinations of system Colors to get the advantages of Colors tuned to a platform while breaking free of the standard palette.
    - Added another demo app

---
+++ ColorKitDemo2/ColorKitDemo2/ColorKitDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  ColorKitDemo2App.swift
+//  ColorKitDemo2
+//
+//  Created by Chon Torres on 5/31/25.
+//
+
+import SwiftUI
+
+@main
+struct ColorKitDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ColorKitDemo2/ColorKitDemo2Tests/ColorKitDemo2Tests.swift
@@ -0,0 +1,16 @@
+//
+//  ColorKitDemo2Tests.swift
+//  ColorKitDemo2Tests
+//
+//  Created by Chon Torres on 5/31/25.
+//
+
+import Testing
+
+struct ColorKitDemo2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
